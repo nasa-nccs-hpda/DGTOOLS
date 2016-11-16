6153ce045cc768ccbf3abd00a9bae7f97c99bd5a
David Shean <dshean@gmail.com>
2016-11-15 20:54:46 -0800

    ntfmos: cleanup

--- dgtools/ntfmos.sh
+++ dgtools/ntfmos.sh
@@ -2,19 +2,10 @@
 
 #David Shean
 #dshean@gmail.com
-#9/3/13
 
-#source ~/.bash_profile
+#Utility to mosaic ntf files by DigitalGlobe CATID
 
-#unset ASPPYMOD
-#unset ASP_PYTHON_MODULES_PATH
-#unset PYTHONPATH
-
-#export LD_LIBRARY_PATH=/u/deshean/src/StereoPipeline/build/lib:/u/deshean/src/visionworkbench/build/lib:/nasa/gcc/4.7.0/lib:/nasa/gcc/4.7.0/lib64
-
-#Utility to mosaic ntf files by CATID
-#Good idea to do this separately, as it only utilizes ~2 cpus
-
+#Function to grab tag from xml
 #Note: can have multiple lines with same tag (e.g. SATID)
 function gettag() {
     xml=$1
@@ -24,11 +15,10 @@ function gettag() {
 
 #Apply the wv_correct L1B CCD offset shifts
 correct=true
+
 #Number of parallel jobs to run - limited by memory
-#wes nodes have 24 GB
 ncpu=4
-#ivy has 64 GB
-#ncpu=8
+
 #Output mosaic ndv
 ndv=0
 
@@ -45,10 +35,6 @@ t_start=$(date +%s)
 
 ext=ntf
 #ext=tif
-#ids=($(ls -i *P1BS*00.$ext | awk -F'[-.]' '{print $3}' | sort -u))
-#ids=($(ls *.ntf | awk -F'[-.]' '{print $3}' | sort -u))
-#This works for existing r100.tif files
-#ids=($(ls *.r100.tif | sed 's/.r100.tif//' | sort -u))
 
 ids=($(dg_get_ids.py .))
 
@@ -56,18 +42,9 @@ ids=($(dg_get_ids.py .))
 ntf_list=''
 for id in ${ids[@]}; do
     outprefix=$id
-    #if $correct ; then
-    #    outprefix=${id}_corr
-    #fi
     if [ -e ${outprefix}.r100.tif ]; then
         echo "Existing mosaic found: $(ls ${outprefix}.r100.tif)"
     else
-        #ntf_list+=" $(ls *[Pp]1[Bb][Ss]*$id*.$ext)"
-        #ntf_list+=" $(ls *$id*.$ext)"
-        #ntf_list+=" $(ls *$id*_P0[0-9][0-9].$ext)"
-        #This is for combined ntf/tif pairs
-        #ntf_list+=" $(ls *$id*_P0[0-9][0-9].{ntf,tif})"
-        #ntf_list+=" $(ls *$id*.$ext | egrep '[Pp]1[Bb][Ss]')"
         #This should catch all combinations of ntf, tif and various filename formats
         ntf_list+=" $(ls | grep -e "${id}" | grep -i P1BS | egrep 'ntf|tif' | grep -v 'corr')"
     fi
@@ -75,7 +52,6 @@ done
 
 if [ ! -z "$ntf_list" ] ; then
     #Check to make sure correction is supported 
-    #xml1=$(echo $ntf_list | awk '{print $1}' | sed "s/${ext}/xml/")
     xml1=$(echo $ntf_list | awk '{print $1}' | awk -F'.' '{print $1 ".xml"}')
     satid=$(gettag $xml1 'SATID')
 
@@ -113,21 +89,14 @@ if [ ! -z "$ntf_list" ] ; then
     mos_opt="--input-nodata-value 5 --output-nodata-value $ndv"
 
     #Fix seam offsets between subscenes
-    #Right now, only have one really bad example, but could be more prevalent for earlier images
-    #WV01_20080913_10200100041CEF00_1020010003338C00
-    #WV1, pre-2009
-    #Maybe look at generation time in xml, enable automatically
     #mos_opt+=" --fix-seams"
 
     for id in ${ids[@]}; do
         outprefix=$id
         if [ ! -e ${outprefix}.r100.tif ]; then
             if $correct ; then
-                #ntf_list=$(ls *[Pp]1[Bb][Ss]*${id}*_corr.tif)
                 ntf_list=$(ls *${id}*_corr.tif)
-                #outprefix=${id}_corr
             else
-                #ntf_list=$(ls *[Pp]1[Bb][Ss]*$id*.ntf)
                 ntf_list=$(ls *$id*.$ext)
             fi
             a="dg_mosaic $mos_opt --output-prefix $outprefix $ntf_list"
