254e100ef91fabc880b52908058a55400502c06f
David Shean <dshean@gmail.com>
2017-01-04 21:15:27 -0800

    nadir_id: update, clean up

--- dgtools/nadir_id.sh
+++ dgtools/nadir_id.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+#This was pulled out of new_dg_stereo.sh
+#Should clean up, likely simpler to port to python
+
 #Extract specified tag from xml file
 function gettag() {
     xml=$1
@@ -12,53 +15,63 @@ dir=$1
 #This will pull out unique catalog IDs from all filenames in a dir
 ids=($(dg_get_ids.py $dir))
 
-#if [ ${#ids[@]} -ne 2 ] ; then
-#    echo "Incorrect number of unique ids in $dir:"
-#    echo ${ids[@]}
-#    exit 1
-#fi
-
-#Determine L and R based on resolution
-#Better to go through all xml files, find avg MEANPRODUCTGSD, but this works
-#Note: this has trouble with other xml files in the dir, like gdalinfo -stats .aux.xml
-#Note: can't use P1BS here when we use existing r100.xml files
-#res1=$(printf '%.3f' $(gettag $(ls *P1BS*${ids[0]}*.xml | grep -v 'aux.xml' | head -1) 'MEANPRODUCTGSD'))
-#res2=$(printf '%.3f' $(gettag $(ls *P1BS*${ids[1]}*.xml | grep -v 'aux.xml' | head -1) 'MEANPRODUCTGSD'))
-#res1=$(printf '%.3f' $(gettag $(ls *${ids[0]}*.xml | grep -v 'aux.xml' | head -1) 'MEANPRODUCTGSD'))
-#res2=$(printf '%.3f' $(gettag $(ls *${ids[1]}*.xml | grep -v 'aux.xml' | head -1) 'MEANPRODUCTGSD'))
-
-xml1=$(ls -t $dir/*${ids[0]}*.xml | grep P1BS | head -1) 
-xml2=$(ls -t $dir/*${ids[1]}*.xml | grep P1BS | head -1) 
-
-#This extracts mean GSD for the mosaiced products
-#Note: some older deliveries are missing MEANPRODUCT tags
-if grep -q MEANPRODUCTGSD $xml1 ; then
-    res1=$(printf '%.2f' $(gettag $xml1 'MEANPRODUCTGSD'))
-else
-    res1=$(printf '%.2f' $(gettag $xml1 'MEANCOLLECTEDGSD'))
-fi
-
-if grep -q MEANPRODUCTGSD $xml2 ; then
-    res2=$(printf '%.2f' $(gettag $xml2 'MEANPRODUCTGSD'))
-else
-    res2=$(printf '%.2f' $(gettag $xml2 'MEANCOLLECTEDGSD'))
-fi
-
-#echo "Input MEANPRODUCTGSD res"
-#echo "${ids[0]}: $res1 GSD"
-#echo "${ids[1]}: $res2 GSD"
-
-#Set L to the image with higher res
-#Note: bc true=1, false=0; bash true=0, false=1
-#if [ $(echo "a=($res1 < $res2); a" | bc -l) ]; then
-if [ $(echo "a=($res1 < $res2); a" | bc -l) -eq 1 ]; then
+if [ "${#ids[@]}" -eq "1" ] ; then
     imgL=${ids[0]}
-    imgR=${ids[1]}
-    res=$res1
-else
-    imgL=${ids[1]}
-    imgR=${ids[0]}
-    res=$res2
+    #xml1=$(ls -t $dir/*${ids[0]}*.xml | grep P1BS | head -1) 
+    xml1=$(ls -t $dir/*${ids[0]}*.xml | head -1)
+    if grep -q MEANPRODUCTGSD $xml1 ; then
+        res=$(printf '%.2f' $(gettag $xml1 'MEANPRODUCTGSD'))
+    else
+        res=$(printf '%.2f' $(gettag $xml1 'MEANCOLLECTEDGSD'))
+    fi
+else if [ "${#ids[@]}" -eq "2" ] ; then
+    #Determine L and R based on resolution
+    #Better to go through all xml files, find avg MEANPRODUCTGSD, but this works
+    #Note: this has trouble with other xml files in the dir, like gdalinfo -stats .aux.xml
+    #Note: can't use P1BS here when we use existing r100.xml files
+    #res1=$(printf '%.3f' $(gettag $(ls *P1BS*${ids[0]}*.xml | grep -v 'aux.xml' | head -1) 'MEANPRODUCTGSD'))
+    #res2=$(printf '%.3f' $(gettag $(ls *P1BS*${ids[1]}*.xml | grep -v 'aux.xml' | head -1) 'MEANPRODUCTGSD'))
+    #res1=$(printf '%.3f' $(gettag $(ls *${ids[0]}*.xml | grep -v 'aux.xml' | head -1) 'MEANPRODUCTGSD'))
+    #res2=$(printf '%.3f' $(gettag $(ls *${ids[1]}*.xml | grep -v 'aux.xml' | head -1) 'MEANPRODUCTGSD'))
+
+    #Limit to subscene xml
+    #xml1=$(ls -t $dir/*${ids[0]}*.xml | grep P1BS | head -1) 
+    #xml2=$(ls -t $dir/*${ids[1]}*.xml | grep P1BS | head -1) 
+
+    xml1=$(ls -t $dir/*${ids[0]}*.xml | head -1) 
+    xml2=$(ls -t $dir/*${ids[1]}*.xml | head -1) 
+
+    #This extracts mean GSD for the mosaiced products
+    #Note: some older deliveries are missing MEANPRODUCT tags
+    if grep -q MEANPRODUCTGSD $xml1 ; then
+        res1=$(printf '%.2f' $(gettag $xml1 'MEANPRODUCTGSD'))
+    else
+        res1=$(printf '%.2f' $(gettag $xml1 'MEANCOLLECTEDGSD'))
+    fi
+
+    if grep -q MEANPRODUCTGSD $xml2 ; then
+        res2=$(printf '%.2f' $(gettag $xml2 'MEANPRODUCTGSD'))
+    else
+        res2=$(printf '%.2f' $(gettag $xml2 'MEANCOLLECTEDGSD'))
+    fi
+
+    #echo "Input MEANPRODUCTGSD res"
+    #echo "${ids[0]}: $res1 GSD"
+    #echo "${ids[1]}: $res2 GSD"
+
+    #Set L to the image with higher res
+    #Note: bc true=1, false=0; bash true=0, false=1
+    if [ $(echo "a=($res1 < $res2); a" | bc -l) -eq 1 ] ; then
+        imgL=${ids[0]}
+        imgR=${ids[1]}
+        res=$res1
+    else
+        imgL=${ids[1]}
+        imgR=${ids[0]}
+        res=$res2
+    fi
+fi
+#Syntax error somewhere???
 fi
 
 echo $imgL $res
