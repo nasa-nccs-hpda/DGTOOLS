1a0fe84ea05f91443fe0cd0cbeb45b298322cf2a
David Shean <dshean@gmail.com>
2017-05-23 21:10:23 -0700

    dg_renamepair: clean up

--- dgtools/dg_renamepair.sh
+++ dgtools/dg_renamepair.sh
@@ -2,6 +2,9 @@
 
 #CD to top dir
 #list=$(ls -d */*PAN)
+#Set output directory
+#outdir=/Volumes/500GB_1/DG_DVD/rename
+#outdir=${PWD}/rename
 #for i in $list; do dg_renamepair.sh $i; done
 
 indir=$1
@@ -16,8 +19,7 @@ ntf1=$(ls *ntf | head -1)
 ntf1_str=$(echo $ntf1 | cut -c 1-13)
 ids=($(dg_get_ids.py .))
 
-outdir=/Volumes/500GB_1/DG_DVD/rename
 pairdir=$outdir/${ntf1_str}_${ids[0]}_${ids[1]}
 
 mkdir -pv $pairdir
-mv *ntf *xml *rename $pairdir
+mv *.ntf *.xml *.rename $pairdir
