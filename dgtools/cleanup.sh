d9495cc9a7a03eceab22cab0414e55d23e8f1779
David Shean <dshean@gmail.com>
2017-07-11 10:39:54 -0700

    cleanup: notes and better log tar generation

--- dgtools/cleanup.sh
+++ dgtools/cleanup.sh
@@ -3,7 +3,9 @@
 #Utility to clean up non-essential intermediate products during and following a large batch ASP run
 
 #qsub -I -q devel -lselect=1:model=bro,walltime=2:00:00
-#parallel --jobs 14 --delay 1 --verbose --progress 'ortho_proc.sh {}' ::: *00
+#parallel --jobs 14 --delay 1 --verbose --progress 'ortho_proc.sh {}' ::: *_1*00
+#Note: Only delete after DEM stage, if running, subsequent stereo steps will fail due to missing input files, even if L an R exist
+#rm $(ls *00/1*_ortho_*m.{tif,xml} | grep -v 30m)
 
 #Dry run
 #rm="echo rm"
@@ -34,8 +36,12 @@ do
     echo
 done
 
-#Tar up log 
-echo "Creating log.tar"
-tar_fn=log.tar
-tar -cf $tar_fn log 
-#$rm -r log
+#If nothing left in queue, tar up logs
+u=deshean
+if ! qstat -u $u | grep -q $u ; then
+    #Tar up log 
+    echo "Creating log.tar"
+    tar_fn=log.tar
+    tar -cf $tar_fn log 
+    #$rm -r log
+fi
